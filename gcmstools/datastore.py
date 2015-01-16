31c327813f0b9a391fa611d4f3e5e0164f7f8120
Ryan Nelson <rnelsonchem@gmail.com>
2015-01-16 09:08:57 -0500

    Added a couple of comments.

--- gcmstools/datastore.py
+++ gcmstools/datastore.py
@@ -4,17 +4,25 @@ import tables as tb
 
 class HDFStore(object):
     def __init__(self, hdfname):
+        # Open the HDF file with pandas
         self.pdh5 = pd.HDFStore(hdfname, mode='a', complevel=9,
                 complib='blosc')
+        # Make a link to the original file as well
         self.h5 = self.pdh5._handle
+        # Se up some compression filters
         self._filters = tb.Filters(complevel=9, complib='blosc')
+        # These are the columns of my stored DataFrame
         self._files_df_columns = ('name', 'filenames')
         
+        # Check to see if the 'files' DataFrame is already saved
         if not hasattr(self.h5.root, 'files'):
+            # Create a blank DF
             df = pd.DataFrame(columns=self._files_df_columns).set_index('name')
             self.pdh5['files'] = df
+        # Reference the DF
         self.files = self.pdh5['files']
 
+        # Check if there is a data group, otherwise create it
         if not hasattr(self.h5.root, 'data'):
             self.h5.create_group('/', 'data', filters=self._filters)
 

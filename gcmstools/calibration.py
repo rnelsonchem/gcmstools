a8961a3a0502d01a42f7619472bd269211ddc2fa
Ryan Nelson <rnelsonchem@gmail.com>
2015-01-16 20:25:14 -0500

    Added plotting functions
    Gave the Calibration class some plotting functions so I could see the data.

--- gcmstools/calibration.py
+++ gcmstools/calibration.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -28,14 +30,22 @@ class Calibrate(object):
     def _proc_group(self, group):
         name, df = group
 
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
         integrals = []
         for idx, series in df.iterrows():
             filename = series['File']
             gcms = self.h5.extract_gcms_data(filename)
             integrals.append(gcms.int_extract(name, series))
-        integrals = np.array(integrals)
+            nameidx = gcms.ref_cpds.index(name)
+            ax.plot(gcms.times, gcms.int_sim[:,nameidx])
+        
+        ax.set_xlim(series['Start'], series['Stop'])
+        fig.savefig(os.path.join('cal', name + '_fits'), dpi=200)
+        fig.clear()
 
         conc = df['Concentration']
+        integrals = np.array(integrals)
 
         std = series['Standard']
         has_std = isinstance(std, str) and not std.isspace()
@@ -44,11 +54,22 @@ class Calibrate(object):
             conc = conc/stdconc
 
         slope, intercept, r, p, stderr = sps.linregress(conc, integrals)
+        self._cal_plot(name, integrals, conc, slope, intercept, r)
 
         caldata = [name, series['Start'], series['Stop'], slope, intercept, r,
                 p, stderr]
 
         return caldata
+
+    def _cal_plot(self, name, integrals, conc, slope, intercept, r):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(conc, slope*conc + intercept, 'k-')
+        ax.plot(conc, integrals, 'o', ms=8)
+        text_string = 'Slope: {:.2f}\nIntercept: {:.2f}\nR^2: {:.5f}'
+        ax.text(0.5, integrals.max()*0.8, text_string.format(slope, intercept, r**2))
+        fig.savefig(os.path.join('cal', name+'_cal_curve'), dpi=200)
+        fig.clear()
 
     def close():
         self.h5.close()

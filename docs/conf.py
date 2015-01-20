5b321f1a10b842a88b70fc4316b7316b97c4729e
Ryan Nelson <rnelsonchem@gmail.com>
2015-01-20 00:42:59 -0500

    Modify index and conf.py, regen PDF
    The title wasn't displaying properly on the HTML pages, so I made a change to
    conf.py to fix that. That required changing the index page. Regen the PDF
    document.

--- docs/conf.py
+++ docs/conf.py
@@ -124,10 +124,10 @@ html_theme_path = ['_templates',]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'gcmstools Documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'gcmstools'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.

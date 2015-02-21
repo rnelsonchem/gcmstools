9eeeead636f916dda1bd9fcf5f0a45ac5991513b
Ryan Nelson <rnelsonchem@gmail.com>
2015-02-21 12:14:55 -0500

    Updated setup.py and docs/conf.py
    I had a couple of mistakes in these files related to the most recent PyPI
    release.

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 from setuptools import setup, find_packages
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name = "gcmstools",
     version = "0.1.1",
 
     description = "Tools for working with GCMS files.",
-    url = "https://github.com/rnelsonchem/gcms_nnls",
+    long_description = long_description,
+    url = "https://github.com/rnelsonchem/gcmstools",
 
     author = "Ryan Nelson",
     author_email = "rnelsonchem@gmail.com",
 
     license = "BSD",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
 
     keywords = "gcms aia nnls",
 

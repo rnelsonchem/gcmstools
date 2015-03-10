124dfb1a679bb6fd4010bed4568c66e640f9d0bc
Ryan Nelson <rnelsonchem@gmail.com>
2015-03-09 21:20:49 -0400

    Made GcmsStore a subclass of pandas.HDFStore
    I don't know why I didn't do this in the first place. It required a lot of
    code changes, but in the end, it actually makes things much simpler. Now I
    don't have to refer to 'pdh5' and 'tbh5' all the time. Unfortunately, if I
    need to use some PyTables functions, I need to refer to the _handle attribute.
    
    Now all of the documenation needs to be updated.

--- gcmstools/datastore.py
+++ gcmstools/datastore.py
@@ -6,34 +6,27 @@ import tables as tb
 
 import gcmstools.filetypes as gcf
 
-class GcmsStore(object):
-    def __init__(self, hdfname='data.h5', quiet=False, **kwargs):
+class GcmsStore(pd.HDFStore):
+    def __init__(self, hdfname, quiet=False, **kwargs):
+        if not 'mode' in kwargs:
+            kwargs['mode'] = 'a'
+        if not 'complevel' in kwargs:
+            kwargs['complevel'] = 9
+        if not 'complib' in kwargs:
+            kwargs['complib'] = 'blosc'
+        super(GcmsStore, self).__init__(hdfname, **kwargs)
+
         self._quiet = quiet
-        self.hdfname = hdfname
-        # Open the HDF file with pandas
-        self.pdh5 = pd.HDFStore(hdfname, mode='a', complevel=9,
-                complib='blosc')
-        # Make a link to the original file as well
-        self.tbh5 = self.pdh5._handle
-        # Se up some compression filters
-        self._filters = tb.Filters(complevel=9, complib='blosc')
+
         # These are the columns of my stored DataFrame
         self._files_df_columns = ('name', 'filename')
         
-        # Check to see if the 'files' DataFrame is already saved
-        if not hasattr(self.pdh5, 'files'):
-            # Create a blank DF
-            df = pd.DataFrame(columns=self._files_df_columns)
-            self.pdh5['files'] = df
-        # Reference the DF
-        self.files = self.pdh5['files']
-
         # Check if there is a data group, otherwise create it
-        if not hasattr(self.tbh5.root, 'data'):
-            self.tbh5.create_group('/', 'data', filters=self._filters)
-        self.data = self.tbh5.root.data
+        if not hasattr(self._handle.root, 'data'):
+            self._handle.create_group('/', 'data', filters=self._filters)
+        self.data = self._handle.root.data
 
-    def append_files(self, datafiles):
+    def append_gcms(self, datafiles):
         '''Append a series of GCMS files into the HDF container.'''
         # Make sure datafiles is iterable
         if not isinstance(datafiles, (tuple, list)):
@@ -42,20 +35,22 @@ class GcmsStore(object):
         names = []
         for data in datafiles:
             filename = data.filename
-            name = self._name_fix(filename)
+            name = self._gcms_name_fix(filename)
             names.append((name, filename))
-            self._append(name, data)
+            self._append_single_gcms(name, data)
         temp_df = pd.DataFrame(names, columns=self._files_df_columns)\
 
-        self.pdh5['files'] = pd.merge(temp_df, self.pdh5['files'],
-                how='outer')
-        self.files = self.pdh5.files
+        if not hasattr(self, 'files'):
+            self.put('files', temp_df)
+        else:
+            self.put('files', pd.merge(temp_df, self.files,
+                    how='outer'))
 
-        self.pdh5.flush()
+        self.flush()
 
-    def extract_data(self, filename):
+    def extract_gcms(self, filename):
         '''Extract a data set from the HDF storage file.'''
-        name = self._name_fix(filename)
+        name = self._gcms_name_fix(filename)
     
         # Find the group and info that corresponds to this file
         group = getattr(self.data, name)
@@ -77,20 +72,21 @@ class GcmsStore(object):
     
         return gcms
     
-    def close(self, ):
+    def compress(self, ):
+        '''Close and compress the HDF file after creation.'''
         # Close the hdf file
-        self.pdh5.close()
+        self.close()
         # Make a copy of the file.
         # This compresses the file if a lot of changes have been made
-        tb.copyFile(self.hdfname, self.hdfname+'temp', overwrite=True)
-        os.remove(self.hdfname)
-        os.rename(self.hdfname+'temp', self.hdfname)
+        tb.copyFile(self.filename, self.filename+'temp', overwrite=True)
+        os.remove(self.filename)
+        os.rename(self.filename+'temp', self.filename)
 
-    def _append(self, name, gcmsobj):
+    def _append_single_gcms(self, name, gcmsobj):
         '''Append a single GCMS file into the HDF container.'''
         # If this exists already, check for equivalence
         if hasattr(self.data, name):
-            different = self._check_data(name, gcmsobj)
+            different = self._check_gcms_data(name, gcmsobj)
             if not different: 
                 if not self._quiet:
                     print("HDF Skipping: {}".format(name))
@@ -99,7 +95,8 @@ class GcmsStore(object):
         if not self._quiet:
             print("HDF Appending: {}".format(name))
         # If not equivalent add the data set to the file
-        group = self.tbh5.create_group('/data', name)
+        group = self._handle.create_group('/data', name, 
+                filters=self._filters)
 
         # Run through the items in the GCMS file
         # Create an info dict for recreating object
@@ -107,14 +104,14 @@ class GcmsStore(object):
         for key, val in gcmsobj.__dict__.items():
             # If they are Numpy arrays, add CArray
             if isinstance(val, np.ndarray):
-                self.tbh5.create_carray(group, key, obj=val,)
+                self._handle.create_carray(group, key, obj=val,)
             # Or else, set a group attribute with the value
             # This is used to check if any changes have been made
             else:
                 gcmsinfo[key] = val
         group._v_attrs['gcmsinfo'] = gcmsinfo
 
-    def _check_data(self, name, obj):
+    def _check_gcms_data(self, name, obj):
         '''Check for equivalence between GCMS file and stored data.'''
         group = getattr(self.data, name)
         groupd = group._v_attrs.gcmsinfo
@@ -134,7 +131,7 @@ class GcmsStore(object):
         # No mismatch
         return False
 
-    def _name_fix(self, badname_path):
+    def _gcms_name_fix(self, badname_path):
         '''Remove special characters from filename.
 
         This is necessary for PyTables natural naming, which is very
